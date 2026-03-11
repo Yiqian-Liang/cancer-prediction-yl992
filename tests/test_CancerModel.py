ddaddd4937da28b399c97a6a338afce47ab2b4e8
Yiqian Liang <yl992@cam.ac.uk>
2026-03-11 19:38:27 +0000

    chore: setup uv, fix linting errors and update dependencies

--- tests/test_CancerModel.py
+++ tests/test_CancerModel.py
@@ -1,25 +1,29 @@
 import unittest
+
 from cancer_prediction_yl992.cancer_model import CancerModel
-class TestCancerModel (unittest.TestCase):
+
+
+class TestCancerModel(unittest.TestCase):
     def test_diagnosis_to_target(self):
-        model=CancerModel()
-        diagnosis='Malignant'
-        target=model.diagnosis_to_target(diagnosis)
-        self.assertEqual(target,0)
-        
-        diagnosis='Benign'
-        target=model.diagnosis_to_target(diagnosis)
-        self.assertEqual(target,1)
+        model = CancerModel()
+        diagnosis = "Malignant"
+        target = model.diagnosis_to_target(diagnosis)
+        self.assertEqual(target, 0)
+
+        diagnosis = "Benign"
+        target = model.diagnosis_to_target(diagnosis)
+        self.assertEqual(target, 1)
 
     def test_target_to_diagnosis(self):
         model = CancerModel()
         target = 0
         diagnosis = model.target_to_diagnosis(target)
-        self.assertEqual(diagnosis, 'Malignant')
+        self.assertEqual(diagnosis, "Malignant")
 
         target = 1
         diagnosis = model.target_to_diagnosis(target)
-        self.assertEqual(diagnosis, 'Benign')
+        self.assertEqual(diagnosis, "Benign")
 
-if __name__ == '__main__':
-    unittest.main()        
+
+if __name__ == "__main__":
+    unittest.main()
